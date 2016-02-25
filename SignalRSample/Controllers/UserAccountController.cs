465264177e7e1797c2e0eaee0d199cdc057f5db0
JRolandros <rolandakamba@yahoo.fr>
2016-02-25 17:48:29 +0100

    create repos

---
+++ SignalRSample/Controllers/UserAccountController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SignalRSample.Models;
+
+namespace SignalRSample.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        private UserDBContext db = new UserDBContext();
+
+        // GET: UserAccount
+        public ActionResult Index()
+        {
+            return View(db.userAccounts.ToList());
+        }
+
+        // GET: UserAccount/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            UserAccount userAccount = db.userAccounts.Find(id);
+
+            if (userAccount == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userAccount);
+        }
+
+        // GET: UserAccount/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserAccount/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,NickName,Email,Phone,Name")] UserAccount userAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                db.userAccounts.Add(userAccount);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userAccount);
+        }
+
+        // GET: UserAccount/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserAccount userAccount = db.userAccounts.Find(id);
+            if (userAccount == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userAccount);
+        }
+
+        // POST: UserAccount/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,NickName,Email,Phone,Name")] UserAccount userAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userAccount).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userAccount);
+        }
+
+        // GET: UserAccount/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserAccount userAccount = db.userAccounts.Find(id);
+            if (userAccount == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userAccount);
+        }
+
+        // POST: UserAccount/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserAccount userAccount = db.userAccounts.Find(id);
+            db.userAccounts.Remove(userAccount);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SignalRSample/Models/UserAccount.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SignalRSample.Models
+{
+    public class UserAccount
+    {
+        private int id;
+        public int Id {
+            get { return id; } 
+            set { id = value; } 
+        }
+
+        private string nickName;
+
+        public string NickName
+        {
+            get { return nickName; }
+            set { nickName = value; }
+        }
+
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        private string phone;
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+            
+    }
+
+
+    public class UserDBContext : DbContext
+    {
+        public DbSet<UserAccount> userAccounts { get; set; }
+    }
+}
